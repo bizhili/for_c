306aaffb53a0e3c83d0478f9e4228de57d6653d8
爱安敝之 <3067842904@qq.com>
2019-08-29 15:09:51 +0800

    update

--- ccf/prj2/main.cpp
+++ ccf/prj2/main.cpp
@@ -1,14 +1,27 @@
 #include <iostream>
+#include "Windows.h"
 using namespace std;
+
+extern int add(int a,int b);
+extern float add(float a,float b);
+extern double add(double a,double b);
+
 int main()
 {
-	int a,b;
-	bool flag;
-	flag= true;
-	cout<<"enter a value"<<endl;
-	cin>>a;
-	if(flag)
-	 cout<<a<<endl;
-	 printf("%d",a);	
-	return 0;
+	cout<<add(3,4)<<endl;
+	cout<<add(3.4,1.0)<<endl;
+	cout<<add(2.3333,2.444)<<endl;
+}
+
+int add(int a,int b)
+{
+	return a+b;
+}
+float add(float a,float b)
+{
+	return a/b;
+}
+double add(double a,double b)
+{
+	return a*b;
 }
