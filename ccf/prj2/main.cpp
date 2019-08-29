a6443692d172f19fda8bc94e56165706f9527ecb
爱安敝之 <3067842904@qq.com>
2019-08-29 10:47:09 +0800

    add prj2 and add instruction txt the difference between c++ and c

---
+++ ccf/prj2/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	bool flag;
+	flag= true;
+	cout<<"enter a value"<<endl;
+	cin>>a;
+	if(flag)
+	 cout<<a<<endl;
+	 printf("%d",a);	
+	return 0;
+}
