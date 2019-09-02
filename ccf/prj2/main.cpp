34f51e74220e3ff95edf6072334c3ca1de0a3850
爱安敝之 <3067842904@qq.com>
2019-09-02 11:12:41 +0800

    added some file about class

--- ccf/prj2/main.cpp
+++ ccf/prj2/main.cpp
@@ -1,27 +1,31 @@
 #include <iostream>
-#include "Windows.h"
+#include<sstream>
 using namespace std;
-
-extern int add(int a,int b);
-extern float add(float a,float b);
-extern double add(double a,double b);
-
+ 
+class complex
+{
+		double re;
+		double im;
+	public:
+		complex(const double r,const double i)
+		{
+			cout<<"To use constructed function"<<endl;
+			re=r+i;
+			im=r-i;
+		}
+		string text()
+		{
+			stringstream ss;
+			ss<<re<<"+"<<im<<"j";
+			return ss.str();
+		}
+};
+ 
 int main()
 {
-	cout<<add(3,4)<<endl;
-	cout<<add(3.4,1.0)<<endl;
-	cout<<add(2.3333,2.444)<<endl;
-}
-
-int add(int a,int b)
-{
-	return a+b;
-}
-float add(float a,float b)
-{
-	return a/b;
-}
-double add(double a,double b)
-{
-	return a*b;
+  	complex a(1,2);
+  	cout<<"a="<<a.text()<<endl;
+  	complex b(3,4);
+  	cout<<"b="<<b.text()<<endl;
+	return 0;
 }
